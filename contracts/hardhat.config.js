13a3eb4c2a9bb9443a18433a63fdc1f4767670a9
Arpit Singh <arpit140940@gmail.com>
2023-12-10 04:30:42 +0530

    config completed

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+    solidity: '0.8.19',
+
+    networks: {
+        testnet: {
+            chainId: 421613,
+            blockConfirmations: 1,
+            url: `${process.env.ARBITRUM_GOERLI_TESTNET}`,
+            accounts: [`${process.env.PRIVATE_KEY}`],
+        },
+    },
 };
